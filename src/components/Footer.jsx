ec5f26773a69750ef7faa12a776427941bb14270
nachbr91 <nabero91@gmail.com>
2022-03-11 17:57:27 +0100

    Edit footer width on large screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import GitHubIcon from '@mui/icons-material/GitHub'
 const Footer = () => {
   return (
     <footer className="bg-blue h-auto" id="contact">
-      <div className="w-[80vw] mx-auto">
+      <div className="w-[85vw] mx-auto md:w-[60vw]">
         <p className="text-center text-white mt-0 mb-[2%] pt-2 md:mb-[1%]">
           Reach me out on my social networks!
         </p>
